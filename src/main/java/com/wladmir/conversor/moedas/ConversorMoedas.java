a91fe702bd1ed1b394886f08ae6b7231f0133ad1
getwlad <wladmir.rrodrigues@gmail.com>
2023-07-27 08:40:18 -0300

    iniciando projeto

---
+++ src/main/java/com/wladmir/conversor/moedas/ConversorMoedas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.wladmir.conversor.moedas;
+
+import com.wladmir.conversor.moedas.view.Menu;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Wladmir Rodrigues
+ */
+public class ConversorMoedas {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
---
+++ src/main/java/com/wladmir/conversor/moedas/view/Teste.java
@@ -0,0 +1,7 @@
+package com.wladmir.conversor.moedas.view;
+
+import javax.swing.*;
+
+public class Teste {
+    private JButton button1;
+}
